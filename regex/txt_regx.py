465ffaa5971b982003b8de43bd0de34f086589d1
zhxh <zhxhcoder@gmail.com>
2020-03-01 15:07:48 +0800

    字符串

--- regex/txt_regx.py
+++ regex/txt_regx.py
@@ -1,10 +1,14 @@
 import re
 
 regex = re.compile(r'\b\w{6}\b')  # 匹配6个字符的单词
-text = regex.search('My number is greats')
+text = regex.search('My number is greats ')
 text.group()  # 调用 group() 返回结果
 
-print(text)
+print(text.group())
 
-m = re.search('(?<=abc)def', 'abcdefaaadef')
-print(m.group(0))
+m = re.match(r"(\w+)\s+(\w+)", "Isaac Newton, physicist")
+print(m.group())
+print(m.group(0))  # The entire match
+print(m.group(2))
+print(m.group(3))
+print(m.group(1, 2))
