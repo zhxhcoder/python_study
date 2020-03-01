1b0005ade82874dabc80f0ceb3814af2cb4808f1
zhxh <zhxhcoder@gmail.com>
2020-03-01 17:22:41 +0800

    获取图片

--- url_lib/page_trace.py
+++ url_lib/page_trace.py
@@ -27,7 +27,7 @@ def get_root_page(pageUrl):
     return page_list
 
 
-for i in range(1, 2):
+for i in range(1, 20):
     url = "https://www.ivsky.com/tupian/meinv_t50/index_" + str(i) + ".html"
     print(url)
     print('第%s页抓到%d个大图首页' % (i, len(get_root_page(url))))
