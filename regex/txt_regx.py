8407efd435734d5c1fdcdb73c6ee1e71156fee16
zhxh <zhxhcoder@gmail.com>
2020-03-01 15:25:11 +0800

    字符串

--- regex/txt_regx.py
+++ regex/txt_regx.py
@@ -1,14 +1,27 @@
 import re
 
+strText = "My number is greats ddd"
 regex = re.compile(r'\b\w{6}\b')  # 匹配6个字符的单词
-text = regex.search('My number is greats ')
+text = regex.search(strText)
 text.group()  # 调用 group() 返回结果
 
 print(text.group())
 
+########################################
+
+listMatch = regex.findall(strText)
+print(listMatch)
+
+for index, strMatch in enumerate(listMatch):
+    print(str(index) + '--' + strMatch)
+########################################
+
 m = re.match(r"(\w+)\s+(\w+)", "Isaac Newton, physicist")
 print(m.group())
-print(m.group(0))  # The entire match
-print(m.group(2))
-print(m.group(3))
-print(m.group(1, 2))
+print(m.group(1))
+
+tGroup = m.group(1, 2)
+print(tGroup)
+
+for strMatch in tGroup:
+    print(strMatch)
