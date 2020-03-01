a4d6f67dee5bd6e5bcd5531ea1d14fed854e453b
zhxh <zhxhcoder@gmail.com>
2020-03-01 14:29:17 +0800

    文件

--- regex/txt_regx.py
+++ regex/txt_regx.py
@@ -1,8 +1,7 @@
 import re
 
 regex = re.compile(r'\b\w{6}\b')  # 匹配6个字符的单词
-regex.search('My phone number is 421-2343-121')
-text = regex.search('My phone number is 421-2343-121')
+text = regex.search('My phone number is 421-234553-121')
 text.group()  # 调用 group() 返回结果
 
-print(text.group())
+print(text)
