7ca6fa5526bf6d5c30a861153f3a992bcdd82e84
zhxh <zhxhcoder@gmail.com>
2020-03-01 18:40:40 +0800

    获取图片

--- file/file_do.py
+++ file/file_do.py
@@ -3,3 +3,13 @@ import os
 os.getcwd()
 
 print(os.getcwd())
+
+f = open("/Users/xhzh/test.txt")  # 在本文件夹下面的一个文件
+
+
+def do_something():
+    pass
+
+
+with open('/Users/xhzh/test.txt', 'r') as f:
+    do_something()
