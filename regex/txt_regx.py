8564ea819e3a61897ca9b38b2370e8b9b655c1e1
zhxh <zhxhcoder@gmail.com>
2020-03-01 18:51:25 +0800

    获取图片

--- regex/txt_regx.py
+++ regex/txt_regx.py
@@ -7,14 +7,15 @@ text.group()  # 调用 group() 返回结果
 
 print(text.group())
 
-########################################
+print("###############################################")
 
 listMatch = regex.findall(strText)
 print(listMatch)
 
 for index, strMatch in enumerate(listMatch):
     print(str(index) + '--' + strMatch)
-########################################
+
+print("###############################################")
 
 m = re.match(r"(\w+)\s+(\w+)", "Isaac Newton, physicist")
 print(m.group())
@@ -25,3 +26,11 @@ print(tGroup)
 
 for strMatch in tGroup:
     print(strMatch)
+
+print("###############################################")
+
+str1="""# print(q)  #<img src="/img.ivsky.com/im.jpg" 
+alt="&#x65F6;&#x5C1A;&#x6444;&#x5F71;&#x56FE;&#x7247;"/> """
+regex1 = re.compile(r'/img.+jpg')  # 匹配6个字符的单词
+
+print(regex1.search(str1).group())
