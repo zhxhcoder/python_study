4bbfe900bde061523cfd804a0d45307f51f66150
zhxh <zhxhcoder@gmail.com>
2020-03-02 15:19:55 +0800

    获取图片

--- file/file_do.py
+++ file/file_do.py
@@ -1,15 +1,28 @@
 import os
+import shutil
 
-os.getcwd()
-
-print(os.getcwd())
-
-f = open("/Users/xhzh/test.txt")  # 在本文件夹下面的一个文件
+print(os.getcwd())  # /usr/local/bin/python3.8 /Users/xhzh/yxFiles/pythonProj/python_study/file/file_do.py
 
 
-def do_something():
-    pass
+def edit_something(file):
+    file.write("第一行\n")
+    file.write("第二行\n")
+    print('写入\n', file.readable())
 
 
-with open('/Users/xhzh/test.txt', 'r') as f:
-    do_something()
+f = open("/Users/xhzh/hello.py", "w+")
+print(f.name)
+shutil.copy('/Users/xhzh/hello.py', '/Users/xhzh/Desktop/first.py')
+
+
+def add_something(file):
+    file.write("第三行\n")
+    file.write("第四行\n")
+    print('添加\n', file.readable())
+
+
+with open('/Users/xhzh/hello.py', 'rw') as f:
+    edit_something(f)
+
+with open('/Users/xhzh/hello.py', 'a') as f:
+    add_something(f)
