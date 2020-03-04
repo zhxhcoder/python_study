7b0124af91787ef002c08273886538c2a1173dcc
zhxh <zhxhcoder@gmail.com>
2020-03-04 17:57:16 +0800

    scrapy_do.py

--- scrapy_lib/scrapy_do.py
+++ scrapy_lib/scrapy_do.py
@@ -2,8 +2,6 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-
     def start_requests(self):
         urls = ["https://mtl.gzhuibei.com/images/img/20713/1.jpg",
                 "https://mtl.gzhuibei.com/images/img/20713/56.jpg"]
@@ -12,8 +10,10 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        print(response.url)
         page = response.url.split("/")[-2]
-        filename = 'pic-%s.pic' % page
+        filename = 'pic-%s.jpg' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+
