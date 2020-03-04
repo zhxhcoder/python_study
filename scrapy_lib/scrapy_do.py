9a92a25c2240a4754081e3f7f4bc04dc0183de13
zhxh <zhxhcoder@gmail.com>
2020-03-04 17:07:28 +0800

    scrapy_do.py

--- scrapy_lib/scrapy_do.py
+++ scrapy_lib/scrapy_do.py
@@ -5,16 +5,15 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
+        urls = ["https://mtl.gzhuibei.com/images/img/20713/1.jpg",
+                "https://mtl.gzhuibei.com/images/img/20713/56.jpg"]
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        filename = 'pic-%s.pic' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
