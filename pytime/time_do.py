2f3937201ffe04aff0b01cdbcd58728beea3fce3
zhxh <zhxhcoder@gmail.com>
2020-03-06 20:18:05 +0800

    时间time

---
+++ pytime/time_do.py
@@ -0,0 +1,6 @@
+import time  # 引入time模块
+
+print("time.time():", time.time())
+print("time.localtime():", time.localtime())
+print("time.gmtime():", time.gmtime())
+print("time.strftime", time.strftime("%Y-%m-%d %H:%M:%S"))
