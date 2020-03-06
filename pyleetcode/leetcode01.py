93744fb675071338f426e26a2c361453c6b567a1
zhxh <zhxhcoder@gmail.com>
2020-03-06 20:44:44 +0800

    leetcode01.py

--- pyleetcode/leetcode01.py
+++ pyleetcode/leetcode01.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def twoSum(nums, target):
     hashmap = {}
     for ind, num in enumerate(nums):
@@ -8,19 +11,20 @@ def twoSum(nums, target):
             return [i, j]
 
 
-def twoSum2(nums, target):
-    lens = len(nums)
-    j = -1
-    for i in range(1, lens):
-        temp = nums[:i]
-        if (target - nums[i]) in temp:
-            j = temp.index(target - nums[i])
-            break
-    if j >= 0:
-        return [j, i]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lens = len(nums)
+        j = -1
+        for i in range(1, lens):
+            temp = nums[:i]
+            if (target - nums[i]) in temp:
+                j = temp.index(target - nums[i])
+                break
+        if j >= 0:
+            return [j, i]
 
 
 nums = [3, 2, 7, 11, 15]
 target = 9
 
-print(twoSum(nums, target))
+print(Solution().twoSum(nums, target))
