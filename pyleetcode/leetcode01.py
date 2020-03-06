dd077ae8a3ad6ebd811f9d0cd74a735f96e59bc5
zhxh <zhxhcoder@gmail.com>
2020-03-06 20:39:39 +0800

    leetcode01.py

--- pyleetcode/leetcode01.py
+++ pyleetcode/leetcode01.py
@@ -1,18 +1,23 @@
 def twoSum(nums, target):
+    hashmap = {}
+    for ind, num in enumerate(nums):
+        hashmap[num] = ind
+    for i, num in enumerate(nums):
+        j = hashmap.get(target - num)
+        if j is not None and i != j:
+            return [i, j]
+
+
+def twoSum2(nums, target):
     lens = len(nums)
     j = -1
-    for i in range(lens):
-        if (target - nums[i]) in nums:
-            if (nums.count(target - nums[i]) == 1) & (
-                    target - nums[i] == nums[i]):  # 如果num2=num1,且nums中只出现了一次，说明找到是num1本身。
-                continue
-            else:
-                j = nums.index(target - nums[i], i + 1)  # index(x,i+1)是从num1后的序列后找num2
-                break
-    if j > 0:
-        return [i, j]
-    else:
-        return []
+    for i in range(1, lens):
+        temp = nums[:i]
+        if (target - nums[i]) in temp:
+            j = temp.index(target - nums[i])
+            break
+    if j >= 0:
+        return [j, i]
 
 
 nums = [3, 2, 7, 11, 15]
