152ad4cccc6172aa60e3fff38215516db59425c7
zhxh <zhxhcoder@gmail.com>
2020-03-06 15:13:40 +0800

    mysql

--- pymsql/init_db.py
+++ pymsql/init_db.py
@@ -2,17 +2,20 @@
 import pymysql
 
 # 创建连接
-conn = pymysql.connect(host='localhost', port=3306, user='root', password='root', database='db_name', charset='utf8mb4')
+conn = pymysql.connect(host='localhost', port=3306, user='root', password='root', database='db_name', charset='UTF8MB4')
 # 创建游标
 cursor = conn.cursor()
 
 # 创建数据库的sql(如果数据库存在就不创建，防止异常)
-sql = "CREATE DATABASE IF NOT EXISTS db_name"
+sql_1 = "CREATE DATABASE IF NOT EXISTS db_name"
 # 执行创建数据库的sql
-cursor.execute(sql)
+cursor.execute(sql_1)
+
+sql_2 = """drop table if exists employee;"""
+cursor.execute(sql_2)
 
 # 创建表
-sql_2 = '''CREATE TABLE `employee` (
+sql_3 = '''CREATE TABLE `employee`(
   `id` INT NOT NULL AUTO_INCREMENT,
   `topic` INT ,
   `ptid` INT NOT NULL,
@@ -25,4 +28,4 @@ sql_2 = '''CREATE TABLE `employee` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 '''
-cursor.execute(sql_2)
+cursor.execute(sql_3)
