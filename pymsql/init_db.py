43795f22a2b4224d8aa1472d35b8831c8c83d0b4
zhxh <zhxhcoder@gmail.com>
2020-03-06 16:08:37 +0800

    db_zhu

--- pymsql/init_db.py
+++ pymsql/init_db.py
@@ -1,4 +1,5 @@
-# python + pymysql 创建数据库
+import time
+
 import pymysql
 
 # 创建连接
@@ -6,29 +7,32 @@ conn = pymysql.connect(host='localhost', port=3306, user='root', password='root'
 # 创建游标
 cursor = conn.cursor()
 
-# 创建数据库的sql(如果数据库存在就不创建，防止异常)
-sql_1 = "CREATE DATABASE IF NOT EXISTS db_zhu"
 # 执行创建数据库的sql
-cursor.execute(sql_1)
-
-sql_2 = """drop table if exists employee;"""
-cursor.execute(sql_2)
-
+# cursor.execute('drop table if exists users;')
 # 创建表
-sql_3 = '''CREATE TABLE `employee`(
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `topic` INT ,
-  `ptid` INT NOT NULL,
-  `level` INT NOT NULL,
-  `time` TIME,
-  `consume` INT NOT NULL,
-  `err` INT NOT NULL,
-  `points` INT NOT NULL,
-  `gid` INT NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-'''
-cursor.execute(sql_3)
+# sql_create_table = '''CREATE TABLE `users`(
+#   `id` INT NOT NULL AUTO_INCREMENT,
+#   `name` CHAR(10) ,
+#   `time` TIME,
+#   PRIMARY KEY (`id`)
+# ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+# '''
+# cursor.execute(sql_create_table)
+
+name = "dudu"
+mTime = time.strftime("%Y-%m-%d %H:%M:%S")
+
+sql_insert = "INSERT INTO users(name, time) VALUES (%s, %s);"
+
+try:
+    # 执行sql语句
+    cursor.execute(sql_insert, [name, mTime])
+    # 提交到数据库执行
+    conn.commit()
+except:
+    # 出现错误 就回滚
+    conn.rollback()
+
 # 关闭光标对象
 cursor.close()
 # 关闭数据库连接
