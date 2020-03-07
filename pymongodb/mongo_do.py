1183e3cba84a471701a25cfe67cc645dd34f2c66
zhxh <zhxhcoder@gmail.com>
2020-03-07 16:35:32 +0800

    mongo

--- pymongodb/mongo_do.py
+++ pymongodb/mongo_do.py
@@ -1,1 +1,8 @@
 import pymongo
+# - mongod --nojournal --dbpath #以控制台方式启动服务器
+# - mongod --install #安装MongoDB以服务方式运行
+# - mongod --help #显示所有的命令选项
+
+client = pymongo.MongoClient(host='localhost', port=27017)
+dblist = client.list_database_names()
+print("db:", dblist)
