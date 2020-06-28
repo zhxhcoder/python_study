26c5be8079dc16adc46c93074a05bf23edcd0c4a
zhxh <zhxhcoder@gmail.com>
2020-06-28 10:55:06 +0800

    jpg照片去重

--- pyfile/file_duplicate_remove.py
+++ pyfile/file_duplicate_remove.py
@@ -1,12 +1,14 @@
 import datetime
 import os
+import re
 import shutil
 
+import exifread
 from tqdm import tqdm
 
 
 # 如果大小一样且，创建时间是在同一分钟
-def is_same_file(file1, file2):
+def is_same_pic(file1, file2):
     file_info1 = os.stat(file1)
     file_info2 = os.stat(file2)
     if file_info1.st_size == file_info2.st_size and get_time_second(file_info1.st_ctime) == get_time_second(
@@ -15,17 +17,20 @@ def is_same_file(file1, file2):
     return False
 
 
-# get_file_hash获得文件的主键
-def get_file_hash(file):
+# get_pic_hash
+def get_pic_hash(file):
     file_info = os.stat(file)
-    return file_info.st_size.__str__() + "/" + get_time_second(file_info.st_ctime)
+    img = exifread.process_file(open(file, 'rb'))
+    time = img['Image DateTime']
+    xsolution = img['Image XResolution']
+    return file_info.st_size.__str__() + "/" + time.__str__() + "/" + xsolution.__str__()
 
 
 def get_time_second(timestamp):
     return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d/%H:%M:%S')
 
 
-def strip_duplicate_file():
+def strip_duplicate_pic():
     src_dir = os.path.abspath(r"/Users/xhzh/yxFiles/_pic/移动硬盘备份/picTest")
 
     dst_dir = os.path.abspath(r"/Users/xhzh/yxFiles/_pic/移动硬盘备份/duplicate")
@@ -42,22 +47,24 @@ def strip_duplicate_file():
         # files 同样是 list, 内容是该文件夹中所有的文件(不包括子目录)
         for root, dirs, files in os.walk(src_dir):
             for file in tqdm(files):
-                file_num = file_num + 1
-                src_file = os.path.join(root, file)
-                file_hash = get_file_hash(src_file)
+                if re.search(r'\.jpg$', file) is not None:
 
-                if file_hash in file_set:
-                    del_num = del_num + 1
-                    shutil.move(src_file, dst_dir)
-                    # os.remove(src_file)
+                    file_num = file_num + 1
+                    src_file = os.path.join(root, file)
+                    file_hash = get_pic_hash(src_file)
 
-                file_set.add(file_hash)
+                    if file_hash in file_set:
+                        del_num = del_num + 1
+                        shutil.move(src_file, dst_dir)
+                        # os.remove(src_file)
 
-                print("--->" + file
-                      + "--->" + file_hash)
+                    file_set.add(file_hash)
 
-    print("文件个数", file_num, "去重文件", del_num)
+                    print("--->" + file
+                          + "--->" + file_hash)
+
+    print("图片个数", file_num, "去重图片", del_num)
 
 
 if __name__ == "__main__":
-    strip_duplicate_file()
+    strip_duplicate_pic()
