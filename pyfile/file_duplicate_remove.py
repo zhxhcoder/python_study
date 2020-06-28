033d95bcdf7b4eb7be0d101944d6539caeb3b2e4
zhxh <zhxhcoder@gmail.com>
2020-06-28 13:42:57 +0800

    jpg照片去重-只筛选能获取拍摄时间的

--- pyfile/file_duplicate_remove.py
+++ pyfile/file_duplicate_remove.py
@@ -47,8 +47,11 @@ def get_time_second(timestamp):
 def strip_duplicate_pic():
     #  src_dir = os.path.abspath(r"G:\移动硬盘备份\_分类备份\照片\jpg")
     #  dst_dir = os.path.abspath(r"G:\移动硬盘备份\_分类备份\照片\duplicate")
+    #  sole_dir = os.path.abspath(r"G:\移动硬盘备份\_分类备份\照片\sole")
+
     src_dir = os.path.abspath(r"/Users/xhzh/yxFiles/_pic/移动硬盘备份/picTest")
     dst_dir = os.path.abspath(r"/Users/xhzh/yxFiles/_pic/移动硬盘备份/duplicate")
+    sole_dir = os.path.abspath(r"/Users/xhzh/yxFiles/_pic/移动硬盘备份/sole")
     # 建立目标目录
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
@@ -89,6 +92,8 @@ def strip_duplicate_pic():
 
                         print("--->" + file
                               + "--->" + file_hash)
+                    else:
+                        shutil.move(src_file, sole_dir)
 
     print("图片个数", file_num, "去重图片", del_num)
 
