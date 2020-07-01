ca95584f6a299f9eaee1c195f7134e5c41018df7
zhxh <zhxhcoder@gmail.com>
2020-07-01 09:42:09 +0800

    视频写入水印

--- pyfile/file_unique_hash.py
+++ pyfile/file_unique_hash.py
@@ -1,6 +1,9 @@
 import datetime
 import hashlib
 import os
+import re
+
+import moviepy.editor as mp
 
 
 def get_file_hash(src_file):
@@ -26,6 +29,23 @@ def get_bytes_hash(src_file, Bytes=1024):
     return ret
 
 
+def water_mark(src_dir):
+    video = mp.VideoFileClip(src_dir)
+
+    logo = (mp.ImageClip("watermark.png")
+            .set_duration(video.duration)  # 时长
+            .resize(height=100)  # 水印高度，等比缩放
+            .margin(left=10, top=10, opacity=1)  # 水印边距和透明度
+            .set_pos(("left", "top")))  # 水印位置
+
+    result = mp.CompositeVideoClip([video, logo])
+
+    print("开始写入水印。。。")
+    # mp4文件默认用libx264编码， 比特率单位bps
+    result.write_videofile(re.sub(r'.{2}\.mp4', "666666.mp4", src_dir), codec="libx264", bitrate="10000000")
+    print("写入完成")
+
+
 def traverse_file(src_dir):
     start = datetime.datetime.now()
     file_num = 0
@@ -34,6 +54,9 @@ def traverse_file(src_dir):
             for file in files:
                 file_num = file_num + 1
                 src_file = os.path.join(root, file)
+                if re.search(r'11\.mp4$', file) is not None:
+                    water_mark(src_file)
+
                 print(
                     file + "-->file_hash-->" + get_file_hash(src_file) + "-->bytes_hash-->" + get_bytes_hash(src_file))
 
