b3a4e9520d7321f9c823e0ab584a63db368bdf9f
zhxh <zhxhcoder@gmail.com>
2020-08-07 18:07:26 +0800

    send_email

--- pysns/email_do.py
+++ pysns/email_do.py
@@ -4,36 +4,38 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-email_user = 'mrcoder@qq.com'
-email_password = ''
-email_send = 'zhxhcoder@qq.com'
 
-# 主题
-subject = '主题-这是Python发送的邮件'
+def send_email(email_send, subject, filename, body):
+    print("邮件发送中...")
 
-msg = MIMEMultipart()
-msg['From'] = email_user
-msg['To'] = email_send
-msg['Subject'] = subject
+    email_user = 'mrcoder@qq.com'
+    email_password = ''
 
-# 附件
-filename = 'QR.png'
-attachment = open(filename, 'rb')
-part = MIMEBase('application', 'octet-stream')
-part.set_payload(attachment.read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= " + filename)
-msg.attach(part)
+    msg = MIMEMultipart()
+    msg['From'] = email_user
+    msg['To'] = email_send
+    msg['Subject'] = subject
 
-# 内容
-body = '内容-这是远程发来的邮件，请注意接收并查收附件' + filename
-msg.attach(MIMEText(body, 'plain'))
+    # 附件
+    if filename is not None:
+        attachment = open(filename, 'rb')
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', "attachment; filename= " + filename)
+        msg.attach(part)
 
-text = msg.as_string()
-server = smtplib.SMTP_SSL('smtp.qq.com', 465)
-# server.starttls()
-server.login(email_user, email_password)
-print("邮件发送...")
-server.sendmail(email_user, email_send, text)
-print("发送成功")
-server.quit()
+    # 内容
+    msg.attach(MIMEText(body, 'plain'))
+
+    text = msg.as_string()
+    server = smtplib.SMTP_SSL('smtp.qq.com', 465)
+    # server.starttls()
+    server.login(email_user, email_password)
+    server.sendmail(email_user, email_send, text)
+    server.quit()
+    print("发送成功")
+
+
+if __name__ == "__main__":
+    send_email('zhxhcoder@qq.com', 'Python发送邮件', 'QR.png', '内容-这是远程发来的邮件，请注意接收并查收附件')
