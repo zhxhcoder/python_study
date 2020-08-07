a4bff99df3122d8ddc62e293029f2ed7ab409626
zhxh <zhxhcoder@gmail.com>
2020-08-07 17:53:39 +0800

    email success

--- pysns/email_do.py
+++ pysns/email_do.py
@@ -8,30 +8,32 @@ email_user = 'mrcoder@qq.com'
 email_password = ''
 email_send = 'zhxhcoder@qq.com'
 
-subject = 'subjec11t'
+# 主题
+subject = '主题-这是Python发送的邮件'
 
 msg = MIMEMultipart()
 msg['From'] = email_user
 msg['To'] = email_send
 msg['Subject'] = subject
 
-body = 'Hi there, sending this email from Python!'
-msg.attach(MIMEText(body, 'plain'))
-
+# 附件
 filename = 'QR.png'
 attachment = open(filename, 'rb')
-
 part = MIMEBase('application', 'octet-stream')
 part.set_payload(attachment.read())
 encoders.encode_base64(part)
 part.add_header('Content-Disposition', "attachment; filename= " + filename)
-
 msg.attach(part)
+
+# 内容
+body = '内容-这是远程发来的邮件，请注意接收并查收附件' + filename
+msg.attach(MIMEText(body, 'plain'))
+
 text = msg.as_string()
 server = smtplib.SMTP_SSL('smtp.qq.com', 465)
 # server.starttls()
 server.login(email_user, email_password)
-
+print("邮件发送...")
 server.sendmail(email_user, email_send, text)
-
+print("发送成功")
 server.quit()
