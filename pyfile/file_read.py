b6a2491de440bdc84c1861a1f1ae3097068f5008
zhxh <zhxhcoder@gmail.com>
2021-01-10 17:17:41 +0800

    读取文件并插入SQLite数据库

--- pyfile/file_read.py
+++ pyfile/file_read.py
@@ -1,7 +1,12 @@
-f = open("/Users/xhzh/Desktop/etfbak8")
-if f is not None:
-    line = f.readline()
-    while line:
-        print(line)
+def readFile(s):
+    f = open(s)
+    if f is not None:
         line = f.readline()
-    f.close()
+        while line:
+            print(line)
+            line = f.readline()
+        f.close()
+
+
+if __name__ == "__main__":
+    readFile("/Users/xhzh/Desktop/etfbak8")
